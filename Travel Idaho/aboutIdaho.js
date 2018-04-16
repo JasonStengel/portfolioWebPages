8e3f0087397fe2e99de79565ece253b4e779b45d
JasonStengel <31941360+JasonStengel@users.noreply.github.com>
2018-04-16 15:30:49 -0600

    error fix and added new page to travel idaho
    fixed some typo's on the resturant page and added the about idaho page to  the  travel website

---
+++ Travel Idaho/aboutIdaho.js
@@ -0,0 +1,21 @@
+
+/*put text here for first text column*/
+$('.grid-container').find('.item1').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium velit sit amet turpis feugiat facilisis vel a nisl. Aenean vel consequat tortor,'+
+' eu pharetra nisi. Aliquam eget mauris in lectus ultrices sodales. Phasellus porttitor arcu a lectus sagittis, ac dignissim est viverra. Quisque dolor felis, malesuada in ipsum sed, fringilla congue lectus. ' + 
+' Quisque venenatis pharetra nibh, id laoreet metus condimentum dapibus. Nulla tempus, sem eu facilisis porttitor, lacus dolor vehicula purus, vitae bibendum nulla lectus non nunc. Proin lorem sapien, egestas at mi ac,' +
+'malesuada semper nulla. Vestibulum nec augue a erat interdum convallis ac ultrices libero.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium velit sit amet turpis feugiat facilisis vel a nisl. Aenean vel ' + 
+'consequat tortor, eu pharetra nisi. Aliquam eget mauris in lectus ultrices sodales. Phasellus porttitor arcu a lectus sagittis, ac dignissim est viverra. Quisque dolor felis, malesuada in ipsum sed, fringilla congue lectus. ' + 
+'Quisque venenatis pharetra nibh, id laoreet metus condimentum dapibus. Nulla tempus, sem eu facilisis porttitor, lacus dolor vehicula purus, vitae bibendum nulla lectus non nunc. Proin lorem sapien, egestas at mi ac, malesuada ' + 
+'semper nulla. Vestibulum nec augue a erat interdum convallis ac ultrices libero. ');
+
+/*put text here for the second text column*/
+$('.grid-container').find('.item3').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium velit sit amet turpis feugiat facilisis vel a nisl. Aenean vel consequat tortor,'+
+' eu pharetra nisi. Aliquam eget mauris in lectus ultrices sodales. Phasellus porttitor arcu a lectus sagittis, ac dignissim est viverra. Quisque dolor felis, malesuada in ipsum sed, fringilla congue lectus. ' + 
+' Quisque venenatis pharetra nibh, id laoreet metus condimentum dapibus. Nulla tempus, sem eu facilisis porttitor, lacus dolor vehicula purus, vitae bibendum nulla lectus non nunc. Proin lorem sapien, egestas at mi ac,' +
+'malesuada semper nulla. Vestibulum nec augue a erat interdum convallis ac ultrices libero.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium velit sit amet turpis feugiat facilisis vel a nisl. Aenean vel ' + 
+'consequat tortor, eu pharetra nisi. Aliquam eget mauris in lectus ultrices sodales. Phasellus porttitor arcu a lectus sagittis, ac dignissim est viverra. Quisque dolor felis, malesuada in ipsum sed, fringilla congue lectus. ' + 
+'Quisque venenatis pharetra nibh, id laoreet metus condimentum dapibus. Nulla tempus, sem eu facilisis porttitor, lacus dolor vehicula purus, vitae bibendum nulla lectus non nunc. Proin lorem sapien, egestas at mi ac, malesuada ' + 
+'semper nulla. Vestibulum nec augue a erat interdum convallis ac ultrices libero. ');
+
+
+
